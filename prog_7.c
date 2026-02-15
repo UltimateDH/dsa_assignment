5ab122932cb51111a7f0ab8be9dea0ca77ddbec8
Sworup Jangam <sworupjangamdh@gmail.com>
2026-02-15 17:36:08 +0545

    dijkstra function created

---
+++ prog_7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define V 5
+#define INF 99999
+
+int minDistance(int dist[],int visited[]){
+    int min=INF, min_index;
+    for(int v=0; v<V; v++){
+        if(!visited[v] && dist[v]<=min){
+            min=dist[v];
+            min_index=v;
+            return min_index;
+        }
+    }
+}
+
+void dijkstra(int graph[V][V], int src) {
+    int dist[V], visited[V]={0};
+
+    for(int i=0;i<V;i++) dist[i]=INF;
+    dist[src]=0;
+
+    for(int count=0; count<V-1; count++){
+        int u=minDistance(dist,visited);
+        visited[u]=1;
+
+        for(int v=0; v<V; v++)
+            if(!visited[v] && graph[u][v] && dist[u]!=INF
+               && dist[u]+graph[u][v]<dist[v])
+                dist[v]=dist[u]+graph[u][v];
+    }
+
+    for(int i=0;i<V;i++)
+        printf("Distance from %d to %d = %d\n",src,i,dist[i]);
+}
