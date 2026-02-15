893956ad26b69120f46846dec437e9cc05abb657
Sworup Jangam <sworupjangamdh@gmail.com>
2026-02-15 17:10:18 +0545

    Question 3 completed

--- prog_3.c
+++ prog_3.c
@@ -5,3 +5,25 @@ struct Node {
     int data;
     struct Node* next;
 };
+
+void reversePrint(struct Node* head) {
+    if (head == NULL) return;
+    reversePrint(head->next);
+    printf("%d ", head->data);
+}
+
+int main() {
+    struct Node *head = NULL, *temp;
+
+    for (int i = 1; i <= 5; i++) {
+        temp = malloc(sizeof(struct Node));
+        temp->data = i;
+        temp->next = head;
+        head = temp;
+    }
+
+    printf("Reverse traversal: ");
+    reversePrint(head);
+
+    return 0;
+}
