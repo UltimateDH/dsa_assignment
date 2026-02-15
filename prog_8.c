aefd64c73f75fd2f300c612587cef96f9b86be58
Sworup Jangam <sworupjangamdh@gmail.com>
2026-02-15 18:00:32 +0545

    Question 8 Complete

--- prog_8.c
+++ prog_8.c
@@ -55,19 +55,22 @@ void insertionSort(int arr[], int n, int *comp, int *swaps) {
 
 /* ---------- Merge Function ---------- */
 void merge(int arr[], int l, int m, int r, int *comp) {
-    int n1 = m-l+1;
-    int n2 = r-m;
 
-    int L[n1], R[n2];
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-    for(int i=0;i<n1;i++)
-        L[i] = arr[l+i];
-    for(int j=0;j<n2;j++)
-        R[j] = arr[m+1+j];
+    int *L = (int*)malloc(n1 * sizeof(int));
+    int *R = (int*)malloc(n2 * sizeof(int));
 
-    int i=0,j=0,k=l;
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[l + i];
 
-    while(i<n1 && j<n2){
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+
+    while(i < n1 && j < n2) {
         (*comp)++;
         if(L[i] <= R[j])
             arr[k++] = L[i++];
@@ -75,9 +78,84 @@ void merge(int arr[], int l, int m, int r, int *comp) {
             arr[k++] = R[j++];
     }
 
-    while(i<n1)
+    while(i < n1)
         arr[k++] = L[i++];
 
-    while(j<n2)
+    while(j < n2)
         arr[k++] = R[j++];
+
+    free(L);
+    free(R);
+}
+
+
+/* ---------- Merge Sort ---------- */
+void mergeSort(int arr[], int l, int r, int *comp) {
+    if(l<r){
+        int m = (l+r)/2;
+        mergeSort(arr,l,m,comp);
+        mergeSort(arr,m+1,r,comp);
+        merge(arr,l,m,r,comp);
+    }
+}
+
+/* ---------- Print Array ---------- */
+void printArray(int arr[], int n) {
+    for(int i=0;i<n;i++)
+        printf("%d ",arr[i]);
+    printf("\n");
+}
+
+/* ---------- Main Program ---------- */
+int main(){
+    int n, choice;
+    int comparisons = 0, swaps = 0;
+
+    printf("Enter N: ");
+    scanf("%d",&n);
+
+    int *arr = (int*)malloc(n * sizeof(int));
+    srand(time(0));
+
+    for(int i=0;i<n;i++)
+        arr[i] = rand()%1000 + 1;
+
+    printf("\nBefore Sorting:\n");
+    printArray(arr,n);
+
+    printf("\nChoose Sorting Algorithm:\n");
+    printf("1. Bubble Sort\n");
+    printf("2. Selection Sort\n");
+    printf("3. Insertion Sort\n");
+    printf("4. Merge Sort\n");
+    printf("Enter choice: ");
+    scanf("%d",&choice);
+
+    switch(choice){
+        case 1:
+            bubbleSort(arr,n,&comparisons,&swaps);
+            break;
+        case 2:
+            selectionSort(arr,n,&comparisons,&swaps);
+            break;
+        case 3:
+            insertionSort(arr,n,&comparisons,&swaps);
+            break;
+        case 4:
+            mergeSort(arr,0,n-1,&comparisons);
+            swaps = 0;  // Merge sort doesn't count swaps traditionally
+            break;
+        default:
+            printf("Invalid choice!");
+            return 0;
+    }
+
+    printf("\nAfter Sorting:\n");
+    printArray(arr,n);
+
+    printf("\nTotal Comparisons: %d\n",comparisons);
+    printf("Total Swaps: %d\n",swaps);
+
+    free(arr);
+    return 0;
 }
