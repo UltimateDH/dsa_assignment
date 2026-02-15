33971d0e2f7d9d52f0c37c40606f24e7364d5015
Sworup Jangam <sworupjangamdh@gmail.com>
2026-02-15 17:11:52 +0545

    Function for inserting, deleting and printing created

--- prog_4.c
+++ prog_4.c
@@ -5,3 +5,36 @@ struct Node {
     int data;
     struct Node *prev, *next;
 };
+
+
+void insertAfter(struct Node* node, int value) {
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = node->next;
+    newNode->prev = node;
+
+    if (node->next != NULL)
+        node->next->prev = newNode;
+
+    node->next = newNode;
+}
+
+void deleteNode(struct Node** head, struct Node* del) {
+    if (*head == del)
+        *head = del->next;
+
+    if (del->next)
+        del->next->prev = del->prev;
+
+    if (del->prev)
+        del->prev->next = del->next;
+
+    free(del);
+}
+
+void printList(struct Node* head) {
+    while (head) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
